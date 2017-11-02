cee284bdb26a2e2421a0537f1ec1294b30dc92c1
rajhimanshu2954 <32230191+rajhimanshu2954@users.noreply.github.com>
2017-11-02 18:31:51 +0530

    plot3.r

---
+++ plot3.r
@@ -0,0 +1,20 @@
+if(!file.exists("./data")){dir.create("./data")}
+url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(url,"data/zipfile.zip")
+unzip(zipfile = "data/zipfile.zip", exdir = "./data")
+household_Power<- read.table("data/household_power_consumption.txt", sep=";", header = T)
+household_Power$Date<- as.Date(household_Power$Date, format = "%d/%m/%Y")
+household_Power_sub<- with(household_Power, household_Power[(Date >= "2007-02-01" & Date <= "2007-02-02"), ])
+summary(household_Power_sub)
+##Plot3
+household_Power_sub$Sub_metering_1<- as.numeric(as.character(household_Power_sub$Sub_metering_1))
+household_Power_sub$Sub_metering_2<- as.numeric(as.character(household_Power_sub$Sub_metering_2))
+household_Power_sub$Sub_metering_3<- as.numeric(as.character(household_Power_sub$Sub_metering_3))
+datetime <- strptime(paste(household_Power_sub$Date, household_Power_sub$Time, sep=" "), "%Y-%m-%d %H:%M:%S")
+par(bg="white")
+png(file="plot3.png",width=480,height=480)
+plot(datetime, household_Power_sub$Sub_metering_1, type = "l", xlab="", ylab = "Energy sub metering")
+lines(datetime, household_Power_sub$Sub_metering_2,type="l",col="red")
+lines(datetime, household_Power_sub$Sub_metering_3,type="l",col="blue")
+legend("topright", legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty=1,lwd=2,col = c("black", "red", "blue"))
+dev.off()
